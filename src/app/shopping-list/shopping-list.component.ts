108374b9a97ea215fc60a1f429842bd2015d1d0c
Mufaddal Vanwala <vanwalamufaddal@gmail.com>
2020-04-29 15:29:12 +0530

    final commit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
 import { Subscription } from "rxjs";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-shopping-list",
@@ -12,7 +13,11 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   ingredients: Ingredient[];
 
-  constructor(private slServive: ShoppingListService) {}
+  constructor(
+    private slServive: ShoppingListService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.ingredients = this.slServive.getIngredients();
@@ -33,5 +38,9 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   onEditItem(index: number) {
     this.slServive.startedEditing.next(index);
+  }
+
+  onBack() {
+    this.router.navigate(["../"], { relativeTo: this.route });
   }
 }
